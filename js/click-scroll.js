8c00497996ff61b963128503880e48998fa2b638
CejeJoe <141450782+CejeJoe@users.noreply.github.com>
2024-06-06 23:25:46 +0300

    Add files via upload

--- js/click-scroll.js
+++ js/click-scroll.js
@@ -1,37 +1,26 @@
 //jquery-click-scroll
-//by syamsul'isul' Arifin
+//by Collin Joe
+$(document).ready(function() {
+    // Initialize all links as inactive
+    $('.navbar-nav .nav-item .nav-link').addClass('inactive');
 
-var sectionArray = [1, 2, 3, 4, 5, 6];
+    // Get the current page pathname
+    var currentPath = window.location.pathname.split("/").pop();
 
-$.each(sectionArray, function(index, value){
-          
-     $(document).scroll(function(){
-         var offsetSection = $('#' + 'section_' + value).offset().top - 90;
-         var docScroll = $(document).scrollTop();
-         var docScroll1 = docScroll + 1;
-         
-        
-         if ( docScroll1 >= offsetSection ){
-             $('.navbar-nav .nav-item .nav-link').removeClass('active');
-             $('.navbar-nav .nav-item .nav-link:link').addClass('inactive');  
-             $('.navbar-nav .nav-item .nav-link').eq(index).addClass('active');
-             $('.navbar-nav .nav-item .nav-link').eq(index).removeClass('inactive');
-         }
-         
-     });
-    
-    $('.click-scroll').eq(index).click(function(e){
-        var offsetClick = $('#' + 'section_' + value).offset().top - 90;
-        e.preventDefault();
-        $('html, body').animate({
-            'scrollTop':offsetClick
-        }, 300)
+    // Define an array with paths and corresponding nav-link indices
+    var pageLinks = [
+        { path: "index.html", index: 0 },
+        { path: "about.html", index: 1 },
+        { path: "works.html", index: 2 },
+        { path: "stories.html", index: 3 },
+        { path: "contact.html", index: 4 },
+        { path: "donate.html", index: 5 }
+    ];
+
+    // Loop through the pageLinks array to find the active link
+    $.each(pageLinks, function(i, page) {
+        if (currentPath === page.path) {
+            $('.navbar-nav .nav-item .nav-link').eq(page.index).addClass('active').removeClass('inactive');
+        }
     });
-    
-});
-
-$(document).ready(function(){
-    $('.navbar-nav .nav-item .nav-link:link').addClass('inactive');    
-    $('.navbar-nav .nav-item .nav-link').eq(0).addClass('active');
-    $('.navbar-nav .nav-item .nav-link:link').eq(0).removeClass('inactive');
 });
